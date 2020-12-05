567e8d3b6d9a303eb900b630a0ea09d3487dfe4c
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2020-12-05 12:50:01 +0000

    add string option to cli

--- index.ts
+++ index.ts
@@ -1,20 +1,31 @@
 #!/usr/bin/env ts-node
 import meow from 'meow';
-import chalk from 'chalk';
-import renderMarkdown from './src/termd';
+import renderMarkdown, { renderString } from './src/termd';
 
-const { input } = meow(`
+const cli = meow(`
   Usage
     $ termd <filename>
 
-  Examples
-    $ termd readme.md
+  Options
+    --string, -s  Use a string with markdown syntax
 
-    ${chalk.bold.underline.red('Heading 1')}
+  Examples
+    $ termd readme.md // # Heading 1
+    ${renderString('# Heading 1')}
+    $ termd -s "# Heading 1"
+    ${renderString('# Heading 1')}
+
 `);
 
 try {
-    console.log(renderMarkdown(input[0]));
+    if (cli.input[0]) {
+        console.log(renderMarkdown(cli.input[0]));
+    }
+
+    if (cli.flags.s || cli.flags.string) {
+        let optionString = (cli.flags.s as string) || (cli.flags.string as string);
+        console.log(renderString(optionString));
+    }
 } catch (error) {
     console.log(error.message);
 }
